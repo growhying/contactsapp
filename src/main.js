f96f85353ad305dabe59ea4fcec9938ea5595cf9
hyyyy2 <hyyyy2@texclub.com>
2018-01-10 01:13:53 +0900

    axios로 접근할 URL 정의

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-//import App from './App'
-import App from './AppAxiosTest.vue'
+import App from './App'
+//import App from './AppAxiosTest.vue'
 import axios from 'axios';
 
 // prototype에 axios를 추가하면 Vue 인스턴스 내부에서 axios를 더 간단하게 사용할 수 있다.
