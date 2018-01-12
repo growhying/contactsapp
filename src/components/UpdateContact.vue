25b39e5a07cdd604fa3a3a53c5066f221ae1b2e7
hyyyy2 <hyyyy2@texclub.com>
2018-01-12 23:00:11 +0900

    입력폼, 수정폼 컴포넌트 작성

---
+++ src/components/UpdateContact.vue
@@ -0,0 +1,15 @@
+<template>
+  <contactForm mode="update" :contact="contact" />
+</template>
+
+<script>
+  import ContactForm from './ContactForm.vue';
+
+  // App.vue로부터 전달받은 속성을 ContactForm.vue에 다시 전달함
+  // ContactForm.vue는 mode가 'update'인 경우에 contact 속성을 전달받아 화면에 뿌려주고 값을 수정할 수 있도록 함
+  export default {
+    name: "updateContact",
+    components: { ContactForm },
+    props: ['contact']
+  }
+</script>
